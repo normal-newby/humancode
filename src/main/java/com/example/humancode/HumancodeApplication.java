97bc110fa704859f9cea34e261c2f7589f653969
Nimo Liu <nimo.r.liu@gmail.com>
2026-09-19 00:03:43 -0400

    init

---
+++ src/main/java/com/example/humancode/HumancodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.humancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumancodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumancodeApplication.class, args);
+	}
+
+}
